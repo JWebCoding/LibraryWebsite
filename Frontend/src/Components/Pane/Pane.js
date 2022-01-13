5edb863e592e6c47e263f26335ef4ec8a32d9553
Joshua Weber <joshua94.weber@gmail.com>
2022-01-12 20:02:21 -0600

    Removed testing code
    The regularly named Pane had a great deal of old testing code that was no longer needed. Deleted it.

--- Frontend/src/Components/Pane/Pane.js
+++ Frontend/src/Components/Pane/Pane.js
@@ -1,72 +1,19 @@
-import React from "react";
+
 import './MainPane';
 import './BookViewPane';
 import './SignInPane';
-import Button from "../Button/Button";
+import React from "react";
 
 // This pane is a testing ground for various ideas.
 
-const Pane =(props)=>{
+const Pane =()=>{
 
-    const printBooks=event=>{
-        event.preventDefault();
-        console.log("Pane TEST");
-    };
-    return(
-        <div id="Pane">
-            <h1>Greetings {props.name}!</h1>
-            <p>{props.text}</p>
-            <form onSubmit={printBooks}>
-                <Button type="submit">Submit</Button>
-            </form>
-        </div>
-    );
-}
-
-// class Pane extends React.Component{
-//     printBooks=event=>{
-//         event.preventDefault();
-//         console.log("Pane TEST");
-//     };
-
-//     render(){
-//         return(
-//         <div id="Pane">
-//             {/* <h1>Greetings {props.name}!</h1>
-//             <p>{props.text}</p> */}
-//             <Button onClick={this.printBooks}> TEST</Button>
-            
-//             <form onSubmit={this.printBooks}>
-//                 <Button type="submit">Submit</Button>
-//             </form>
-//         </div>
-//         )
-//     }
-// }
-
-// class Pane extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {isToggleOn: true};
-
-//     // This binding is necessary to make `this` work in the callback
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-
-//   handleClick() {
-//     this.setState(prevState => ({
-//       isToggleOn: !prevState.isToggleOn
-//     }));
-//   }
-
-//   render() {
-//     return (
-//       <button onClick={this.handleClick}>
-//         {this.state.isToggleOn ? 'ON' : 'OFF'}
-//       </button>
-//     );
-//   }
-// }
-
+  return (
+    <div className="app">
+      <div className="time">
+      </div>
+    </div>
+  );
+};
 
 export default Pane;
