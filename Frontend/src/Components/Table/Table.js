7e5d9c5de2cfaafc97826f69edc77beb4435980c
Joshua Weber <joshua94.weber@gmail.com>
2022-01-27 19:45:57 -0600

    Sorting added
    Added the ability to sort table content

--- Frontend/src/Components/Table/Table.js
+++ Frontend/src/Components/Table/Table.js
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { useTable } from "react-table";
+import { useTable, useSortBy } from "react-table";
 import "./Table.css";
 
-const Table = ({columns,data}) => {
-
+const Table = ({ columns, data }) => {
   // const [loading, setLoading] = useState(true);
 
   // if (seconds === 3) {
@@ -12,49 +11,44 @@ const Table = ({columns,data}) => {
   //   setLoading(true);
   // }
 
-  const {
-    getTableProps,
-    getTableBodyProps,
-    headerGroups,
-    rows,
-    prepareRow, 
-  } = useTable({columns,data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data }, useSortBy);
 
   return (
-      <table {...getTableProps()}>
+    <table {...getTableProps()}>
       <thead>
-        {headerGroups.map(headerGroup => (
+        {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps([
-                {
-                  className: column.className,
-                }
-              ])}>
-                {column.render('Header')}
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps(
+                  column.getSortByToggleProps()
+                )}
+              >
+                {column.render("Header")}
+                <span>
+                  {/* {column.isSorted ? (column.isSortedDesc ? " 🔽" : " 🔼") : ""} */}
+                  {column.isSorted ? (column.isSortedDesc ? " ↓" : " ↑") : ""}
+                </span>
               </th>
             ))}
           </tr>
         ))}
       </thead>
       <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-          prepareRow(row)
+        {rows.map((row) => {
+          prepareRow(row);
           return (
             <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <td {...cell.getCellProps()}>
-                    {cell.render('Cell')}
-                  </td>
-                )
+              {row.cells.map((cell) => {
+                return <td {...cell.getCellProps()}>{cell.render("Cell")}</td>;
               })}
             </tr>
-          )
+          );
         })}
       </tbody>
     </table>
-  )
+  );
 };
 
 export default Table;
