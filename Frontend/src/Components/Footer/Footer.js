49f77b31339b75ae3b320a77e67b236722bd993e
Joshua Weber <joshua94.weber@gmail.com>
2022-03-31 21:36:21 -0500

    Updated copyright
    Changed the copyright to the current year.

--- Frontend/src/Components/Footer/Footer.js
+++ Frontend/src/Components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = props =>{
             <div id="footerText">
                 <p>Place holder page </p>
                 <p>Property of Joshua Weber</p>
-                <p>Copyright 2021</p>
+                <p>Copyright 2022</p>
             </div>
         </footer>
     );
