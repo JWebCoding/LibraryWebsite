2688e512cb216b40b9fcec31a39de63e9308e3cd
Joshua Weber <joshua94.weber@gmail.com>
2022-03-31 21:33:10 -0500

    Added Search Bar
    Added a search bar to the BookView pane.
    
    Entirely aesthetic and currently does nothing. Logic will be added later.

--- Frontend/src/Components/Pane/BookViewPane.js
+++ Frontend/src/Components/Pane/BookViewPane.js
@@ -1,20 +1,29 @@
 import React from "react";
 import Table from "../Table/Table";
 import "./BookViewPane.css";
-import {data, columns} from "../../Data/TableData";
+import { data, columns } from "../../Data/TableData";
 
 const BookViewPane = (props) => {
-  
-  const tableData= React.useMemo(()=> data,[]);
-  const tableColumns= React.useMemo(()=> columns,[]);
-  
+  const tableData = React.useMemo(() => data, []);
+  const tableColumns = React.useMemo(() => columns, []);
+
+  const bookDetailHandler = () => {
+    props.openBookDetailsModal();
+  };
+
   return (
     <React.Fragment>
       <div id="BookViewPane">
         <h1 id="PaneTitle">The Collection</h1>
+        <div id="Search-Bar">
+          <input className="searchBar" />
+          <button>Search</button>
+        </div>
         <div id="Table-and-Controls">
           <div id="Table-Buttons">
-            <button>View Book Details</button>
+            <button onClick={() => bookDetailHandler()}>
+              View Book Details
+            </button>
             <button>Refresh Table Data</button>
             <button>Reset Table</button>
           </div>
