4c65b19d85d253d9147cb41dc4dcbcb3bd02cbd9
Joshua Weber <joshua94.weber@gmail.com>
2022-04-30 22:34:21 -0500

    Spring Boot Server
    These will be the new files used for the backend. Everything will now be done in Java Spring Boot.

---
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/LibraryWebsiteApplication.java
@@ -0,0 +1,13 @@
+package jweb.coding.LibraryWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryWebsiteApplication.class, args);
+	}
+
+}
---
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/LibraryWebsiteController.java
@@ -0,0 +1,13 @@
+package jweb.coding.LibraryWebsite;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LibraryWebsiteController {
+	
+//	@RequestMapping(value = "/")
+//	public String index() {
+//		return "index";
+//	}
+}
+
