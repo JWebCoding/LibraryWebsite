fe3cdae7bf43ebf31edc30a6690cf63f9ff1db9e
Joshua Weber <joshua94.weber@gmail.com>
2022-05-10 20:21:31 -0500

    Updated to React 18 & and removed React Router
    Implemented React 18 to ensure that the project was up-to-date and removed React Router to avoid a nesting issue in App.js that was caused by its existence here.

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {HashRouter} from 'react-router-dom'
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById('root'))
+  console.log(React.version);
+  const container = document.getElementById('root');
+  const root = createRoot(container); 
+  root.render(<App tab="home" />);
 ;
