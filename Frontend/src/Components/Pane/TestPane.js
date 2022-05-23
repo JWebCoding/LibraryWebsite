f36f69f72852230884a8b25cfb15e07161af7b20
Joshua Weber <joshua94.weber@gmail.com>
2022-05-22 20:13:58 -0500

    Created TestPane
    Added a test pane for experimentation purposes.

---
+++ Frontend/src/Components/Pane/TestPane.js
@@ -0,0 +1,41 @@
+import React from "react";
+import libraryService from "../../services/library.service";
+import commands from "../../commands";
+import axios from "axios";
+
+// This pane is a testing ground for various ideas.
+
+const TestPane =()=>{
+
+  const[bookTitle, setBookTitle] = React.useState();
+  const[bookInfo, setBookInfo] = React.useState({});
+  const[bookID, setBookID] = React.useState(null);
+
+  const getBook = async () => {
+    try{
+      // const response = await axios.get(commands.getSpecificBook(bookID));
+      const response = await axios.get(commands.getTenBooks());
+      console.log(response)
+      // setBookInfo(response.data[0])
+    } catch(error) {
+  
+    }
+  }
+
+  function handleChange(event){
+    setBookID(event.target.value);
+  }
+
+  return (
+    <div id="TestPane">
+      <h1>TESTING AREA</h1>
+      <main>
+        <input id="bookIdField" onChange={handleChange}></input>
+        <button id="submitButton" onClick={()=>getBook()}>Get Book</button>
+        <h1 id="bookTitleField">{bookInfo.title}</h1>
+      </main>
+    </div>
+  );
+};
+
+export default TestPane;
