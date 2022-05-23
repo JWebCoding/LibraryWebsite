cfb323dcc053c48f5fba3f36f628ac458a5e38c7
Joshua Weber <joshua94.weber@gmail.com>
2022-05-22 20:25:18 -0500

    Added Query for finding ten books
    Added a query for finding the last ten books in the database as well as making changes to imports

--- Backend/src/main/java/jweb/coding/LibraryWebsite/Repsoitories/BookRepository.java
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/Repsoitories/BookRepository.java
@@ -1,13 +1,20 @@
 package jweb.coding.LibraryWebsite.Repsoitories;
 import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+
+import com.mysql.cj.Session;
+
 import jweb.coding.LibraryWebsite.Models.Book;
-import jweb.coding.LibraryWebsite.Models.Genre;
 
 public interface BookRepository extends CrudRepository<Book, Integer>{
 	List<Book> findAll();
 	
 	List<Book> findBybookID(int bookID);
+	
+	@Query(nativeQuery=true, value= "Select * from library.Book b where genreID = 8 order by b.bookID Desc Limit 0,10")
+	List<Book> findLastTen();
 	
 }
 
