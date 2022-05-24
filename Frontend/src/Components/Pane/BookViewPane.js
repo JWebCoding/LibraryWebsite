d3fa5cae9587c57229e0f4bf650456b89f693031
Joshua Weber <joshua94.weber@gmail.com>
2022-05-24 00:35:35 -0500

    Added API calls to server
    Added the ability to make a GET request in the BookViewPane. Not complete but a very important step.

--- Frontend/src/Components/Pane/BookViewPane.js
+++ Frontend/src/Components/Pane/BookViewPane.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Table from "../Table/Table";
 import "./BookViewPane.css";
-import { data, columns } from "../../Data/TableData";
+import { columns } from "../../Data/TableData";
+import LibraryDataService from "../../services/library.service";
+import commands from "../../commands";
+import axios from "axios";
+import libraryService from "../../services/library.service";
 
 const BookViewPane = (props) => {
-  const tableData = React.useMemo(() => data, []);
+
+  // Hooks
+  const [bookInfo, setBookInfo] = React.useState([]);
   const tableColumns = React.useMemo(() => columns, []);
+
+  useEffect(() => {(async () => {
+    try{
+      const response = await axios.get(commands.getTenBooks());
+      setBookInfo(response.data);
+
+    } catch(e) {
+      console.log("Error", e.stack);
+      console.log("Error", e.name);
+      console.log("Error", e.message);
+    }
+    })();
+  }, []);
+
+  const refreshTableHandler = async () => {
+    try{
+      const allbooks = LibraryDataService.getAll();
+      console.log((await allbooks).data);
+
+    } catch(e) {
+      console.log("Error", e.stack);
+      console.log("Error", e.name);
+      console.log("Error", e.message);
+    }
+  }
 
   const bookDetailHandler = () => {
     props.openBookDetailsModal();
@@ -24,11 +55,11 @@ const BookViewPane = (props) => {
             <button onClick={() => bookDetailHandler()}>
               View Book Details
             </button>
-            <button>Refresh Table Data</button>
+            <button onClick={()=>refreshTableHandler()}>Refresh Table Data</button>
             <button>Reset Table</button>
           </div>
           <div id="BookTable">
-            <Table data={tableData} columns={tableColumns} />
+            <Table data={bookInfo} columns={tableColumns} />
           </div>
         </div>
       </div>
