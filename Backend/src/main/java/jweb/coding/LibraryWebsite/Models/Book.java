4ce2b8dd236e83c620f00b51018de45e1d06c66a
Joshua Weber <joshua94.weber@gmail.com>
2022-06-02 19:59:47 -0500

    Changed the book class to represent a complete book
    Converted the book class to represented  complete book rather than the incomplete book information inside the book table on the database. It was decided that using this version of the book that was created during the query of the database was more effective and would meet the needs of the website barring additional details which may be added in the future.

--- Backend/src/main/java/jweb/coding/LibraryWebsite/Models/Book.java
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/Models/Book.java
@@ -12,105 +12,77 @@ public class Book {
 	int bookID;
 	
 	String title;
-	String notes;
+	String genre_name;
+	String publisher_name;
+	String series_name;
+	String first_name;
 	
-	int authorID;
-	int genreID;
-	int seriesID;
-	int languageID;
 	int copyright;
-	int pages;
-	int format;
-	int publisherID;
 	
-	Integer series_part;
-	Integer edition;
-	
-	long isbn;
+	Long isbn;
 
 	public int getBookID() {
 		return bookID;
 	}
+
 	public void setBookID(int bookID) {
 		this.bookID = bookID;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	public String getNotes() {
-		return notes;
+
+	public String getSeries_name() {
+		return series_name;
 	}
-	public void setNotes(String notes) {
-		this.notes = notes;
+
+	public void setSeries_name(String series_name) {
+		this.series_name = series_name;
 	}
-	public int getAuthorID() {
-		return authorID;
+
+	public String getFirst_name() {
+		return first_name;
 	}
-	public void setAuthorID(int authorID) {
-		this.authorID = authorID;
+
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
 	}
-	public int getGenreID() {
-		return genreID;
-	}
-	public void setGenreID(int genreID) {
-		this.genreID = genreID;
-	}
-	public int getSeriesID() {
-		return seriesID;
-	}
-	public void setSeriesID(int seriesID) {
-		this.seriesID = seriesID;
-	}
-	public int getLanguageID() {
-		return languageID;
-	}
-	public void setLanguageID(int languageID) {
-		this.languageID = languageID;
-	}
-	public Integer getSeries_part() {
-		return series_part;
-	}
-	public void setSeries_part(Integer series_part) {
-		this.series_part = series_part;
-	}
-	public Integer getEdition() {
-		return edition;
-	}
-	public void setEdition(Integer edition) {
-		this.edition = edition;
-	}
-	public int getCopyright() {
-		return copyright;
-	}
-	public void setCopyright(int copyright) {
-		this.copyright = copyright;
-	}
-	public int getPages() {
-		return pages;
-	}
-	public void setPages(int pages) {
-		this.pages = pages;
-	}
-	public int getFormat() {
-		return format;
-	}
-	public void setFormat(int format) {
-		this.format = format;
-	}
-	public int getPublisherID() {
-		return publisherID;
-	}
-	public void setPublisherID(int publisherID) {
-		this.publisherID = publisherID;
-	}
+
 	public long getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(long isbn) {
 		this.isbn = isbn;
 	}
-	
+
+	public String getGenre_name() {
+		return genre_name;
+	}
+
+	public void setGenre_name(String genre_name) {
+		this.genre_name = genre_name;
+	}
+
+	public String getPublisher_name() {
+		return publisher_name;
+	}
+
+	public void setPublisher_name(String publisher_name) {
+		this.publisher_name = publisher_name;
+	}
+
+	public int getCopyright() {
+		return copyright;
+	}
+
+	public void setCopyright(int copyright) {
+		this.copyright = copyright;
+	}
+
 }
