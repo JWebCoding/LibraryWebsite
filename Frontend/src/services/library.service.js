e3c2f80fabdd14da3cd487574644e0830c776787
Joshua Weber <joshua94.weber@gmail.com>
2022-06-02 20:10:13 -0500

    Update library.service.js
    Added the command for getting ten books to the services file so that the website can utilize the API.

--- Frontend/src/services/library.service.js
+++ Frontend/src/services/library.service.js
@@ -4,6 +4,10 @@ class LibraryDataService {
   getAll() {
     return http.get("/books");
   }
+
+  getTenBooks() {
+    return http.get("/books10");
+  }
   getSpecificBook(id) {
     return http.get(`/books/${id}`);
   }
