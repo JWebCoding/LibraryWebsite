fab5d6b57a0a241e4f849ab577905eb45643015b
Joshua Weber <joshua94.weber@gmail.com>
2022-06-02 20:07:40 -0500

    Create TableColumns.js
    New file with all the information for the columns and only for them.

---
+++ Frontend/src/Data/TableColumns.js
@@ -0,0 +1,38 @@
+// Holds the data and columns for the BookView Table.
+export const columns = [
+  {
+    Header: "Title",
+    accessor: "title",
+    width: 250,
+  },
+  {
+    Header: "Author",
+    accessor: "first_name",
+    width: 150,
+  },
+  {
+    Header: "Genre",
+    accessor: "genre_name",
+    width: 100,
+  },
+  {
+    Header: "Publisher",
+    accessor: "publisher_name",
+    width: 150,
+  },
+  {
+    Header: "CopyRight",
+    accessor: "copyright",
+    width: 100,
+  },
+  {
+    Header: "ISBN",
+    accessor: "isbn",
+    width: 100,
+  },
+  {
+    Header: "Format",
+    accessor: "format",
+    width: 50,
+  },
+];
