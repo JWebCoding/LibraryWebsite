55cdac2a9bf1772d5304f2fa971c7264c145ec6e
Joshua Weber <joshua94.weber@gmail.com>
2022-06-02 20:05:57 -0500

    Added queries for new API calls
    Added the needed methods for the controller to be able to query the database for the information that  is needed by the website.
    Also created a "baseQuery" for use in any other query that will need it. This can be added on to in order to create more specific calls if needed.

--- Backend/src/main/java/jweb/coding/LibraryWebsite/Repsoitories/BookRepository.java
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/Repsoitories/BookRepository.java
@@ -1,20 +1,24 @@
 package jweb.coding.LibraryWebsite.Repsoitories;
-import java.util.List;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import com.mysql.cj.Session;
 
 import jweb.coding.LibraryWebsite.Models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Integer>{
-	List<Book> findAll();
+	
+	String baseQuery="select bookID,title,s.series_name,series_part,a.first_name,middle_name,last_name,p.publisher_name,isbn,edition,copyright,g.genre_type,g.genre_name,format,pages,l.language_name,notes from book join language l on book.languageID = l.languageID join publisher p on book.publisherID = p.publisherID join author a on book.authorID = a.authorID join series s on book.seriesID = s.seriesID join genre g on book.genreID = g.genreID";
+//	@Procedure("Get_All_Books")
+	@Query(nativeQuery=true, value= baseQuery)
+	List<Book> getAllBooks();
 	
 	List<Book> findBybookID(int bookID);
 	
-	@Query(nativeQuery=true, value= "Select * from library.Book b where genreID = 8 order by b.bookID Desc Limit 0,10")
-	List<Book> findLastTen();
+	@Query(nativeQuery=true, value= baseQuery+" order by bookID desc limit 10;")
+	List<Book> getLastTen();
+	
+	
 	
 }
 
