20826a95e0dbc8aa56afe7a4cf5dbf91bfcb8dd3
Joshua Weber <joshua94.weber@gmail.com>
2022-06-11 13:31:40 -0500

    Set the services to use the new API URLS
    Set the classes responsible for calling APIs to use the up-to-date URLs and added "searchForBooks" as well.

--- Frontend/src/services/library.service.js
+++ Frontend/src/services/library.service.js
@@ -2,14 +2,17 @@ import http from "../http-common";
 class LibraryDataService {
 
   getAll() {
-    return http.get("/books");
+    return http.get("/all");
   }
 
   getTenBooks() {
-    return http.get("/books10");
+    return http.get("/ten");
   }
   getSpecificBook(id) {
-    return http.get(`/books/${id}`);
+    return http.get(`/specific/${id}`);
+  }
+  searchForBooks(term) {
+    return http.get(`/search/${term}`);
   }
 }
 export default new LibraryDataService();
