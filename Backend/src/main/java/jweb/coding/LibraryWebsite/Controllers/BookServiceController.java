9a84cdbf9c24819dcf9e0c7ecd9b24873204d04a
Joshua Weber <joshua94.weber@gmail.com>
2022-06-19 02:01:11 -0500

    Removed unneeded imports & Added a transactional annotation

--- Backend/src/main/java/jweb/coding/LibraryWebsite/Controllers/BookServiceController.java
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/Controllers/BookServiceController.java
@@ -1,8 +1,5 @@
 package jweb.coding.LibraryWebsite.Controllers;
 
-import java.util.List;
-
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +7,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-
-import jweb.coding.LibraryWebsite.Services.BookServiceImpl;
 import jweb.coding.LibraryWebsite.Interfaces.BookService;
-import jweb.coding.LibraryWebsite.Models.Book;
 
 @RestController
 @RequestMapping(path="/books")
@@ -28,6 +20,7 @@ public class BookServiceController {
 	@Autowired BookService bookServicesImpl;
 	
 	@GetMapping(path="/all")
+	@Transactional(readOnly = false)
 	public ResponseEntity<Object> getBooks() {
 		return new ResponseEntity<>(bookServicesImpl.getBooks(), HttpStatus.OK);
 	}
