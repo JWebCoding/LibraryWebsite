f6cfe34edf78fb8633bf6371a9d833b2bcff3368
Joshua Weber <joshua94.weber@gmail.com>
2022-06-19 01:55:44 -0500

    Added new variables
    Added all of the new variables needed for books in the website.

--- Backend/src/main/java/jweb/coding/LibraryWebsite/Models/Book.java
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/Models/Book.java
@@ -16,74 +16,92 @@ public class Book {
 	int bookID;
 	
 	String title;
-	String genre_name;
-	String publisher_name;
 	String series_name;
 	String author;
-	
-	@Transient
 	String first_name;
-	@Transient
 	String middle_name;
-	@Transient
 	String last_name;
+	String publisher_name;
+	String isbn;
+	String genre_name;
+	String notes;
+	
 
-	int copyright;
+	Integer series_part;
+	Integer edition;
+	int genre_type;
 	int format;
 	int pages;
 	
-	String isbn;
-
+	int authorID;
+	int publisherID;
+	int seriesID;
+	int genreID;
+	int copyright;
+	
 	public int getBookID() {
 		return bookID;
 	}
-
 	public String getTitle() {
 		return title;
 	}
-
-	public String getGenre_name() {
-		return genre_name;
-	}
-
-	public String getPublisher_name() {
-		return publisher_name;
-	}
-
 	public String getSeries_name() {
 		return series_name;
 	}
-
 	public String getAuthor() {
 		return author;
 	}
-
 	public String getFirst_name() {
 		return first_name;
 	}
-
 	public String getMiddle_name() {
 		return middle_name;
 	}
-
 	public String getLast_name() {
 		return last_name;
 	}
-
+	public String getPublisher_name() {
+		return publisher_name;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public String getGenre_name() {
+		return genre_name;
+	}
+	public String getNotes() {
+		return notes;
+	}
+	public Integer getSeries_part() {
+		return series_part;
+	}
+	public Integer getEdition() {
+		return edition;
+	}
+	public int getGenre_type() {
+		return genre_type;
+	}
+	public Integer getFormat() {
+		return format;
+	}
+	public int getPages() {
+		return pages;
+	}
+	public int getAuthorID() {
+		return authorID;
+	}
+	public int getPublisherID() {
+		return publisherID;
+	}
+	public int getSeriesID() {
+		return seriesID;
+	}
+	public int getGenreID() {
+		return genreID;
+	}
 	public int getCopyright() {
 		return copyright;
 	}
 
-	public int getFormat() {
-		return format;
-	}
-
-	public int getPages() {
-		return pages;
-	}
-
-	public String getIsbn() {
-		return isbn;
-	}
-
+	
 }
