d18fd4bd517743d76468d0f26d0dbc5ecc6d9ba1
Joshua Weber <joshua94.weber@gmail.com>
2022-07-07 19:31:53 -0500

    Changed the return type of createGenre

--- Backend/src/main/java/jweb/coding/LibraryWebsite/Interfaces/GenreService.java
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/Interfaces/GenreService.java
@@ -1,10 +1,12 @@
 package jweb.coding.LibraryWebsite.Interfaces;
 
 import java.util.List;
+import java.util.Optional;
 
 import jweb.coding.LibraryWebsite.Models.Genre;
 
 public interface GenreService {
-	public abstract void createGenre(Genre genre);
+	public abstract Genre createGenre(Genre genre);
+	public abstract List<Genre> getSpecificGenre(int id);
 	public abstract List<Genre> getGenres();
 }
