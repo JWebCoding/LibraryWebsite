4bce66617d8dfc5b1ba91c6f4a2881c5ccd4965e
Joshua Weber <joshua94.weber@gmail.com>
2022-07-07 19:38:43 -0500

    Changed URLs for mappings

--- Frontend/src/services/library.service.js
+++ Frontend/src/services/library.service.js
@@ -1,18 +1,46 @@
-import http from "../http-common";
+import http from "./http-common";
 class LibraryDataService {
 
-  getAll() {
-    return http.get("/all");
+  getAllAuthors() {
+    return http.get("/authors/all");
+  }
+
+  getAllBooks() {
+    return http.get("/books/all");
+  }
+
+  getAllGenres() {
+    return http.get("/genres/all");
+  }
+
+  getAllLanguages() {
+    return http.get("/languages/all");
+  }
+
+  getAllPublishers() {
+    return http.get("/publishers/all");
+  }
+
+  getAllSeries() {
+    return http.get("/series/all");
   }
 
   getTenBooks() {
-    return http.get("/ten");
+    return http.get("/books/ten");
   }
   getSpecificBook(id) {
-    return http.get(`/specific/${id}`);
+    return http.get(`/books/specific/${id}`);
   }
   searchForBooks(term) {
-    return http.get(`/search/${term}`);
+    return http.get(`/books/search/${term}`);
   }
+
+  createNewBook(object) {
+    return http.post("/books/create", {object});
+  }
+
+  createNewAuthor(object) {
+    return http.post("/authors/create", {object});
+  } 
 }
 export default new LibraryDataService();
