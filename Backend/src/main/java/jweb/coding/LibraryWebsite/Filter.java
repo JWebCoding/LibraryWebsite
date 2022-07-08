cb78286cfe034022e28edfc1cffc2db1f9dfa914
Joshua Weber <joshua94.weber@gmail.com>
2022-07-07 19:34:50 -0500

    Added a filter class
    Created a filter class to log requests and responses to and from the server.

---
+++ Backend/src/main/java/jweb/coding/LibraryWebsite/Filter.java
@@ -0,0 +1,18 @@
+package jweb.coding.LibraryWebsite;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class Filter {
+	@Bean
+	public CommonsRequestLoggingFilter requestLoggingFilter() {
+	    CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+	    loggingFilter.setIncludeClientInfo(true);
+	    loggingFilter.setIncludeQueryString(true);
+	    loggingFilter.setIncludePayload(true);
+	    loggingFilter.setMaxPayloadLength(64000);
+	    return loggingFilter;
+	}
+}
